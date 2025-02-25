85c07724e6e4d09c605464dba14be44311deda88
ethanbeanstalk <ethan@beanstalkwebsolutions.com>
2025-02-24 19:04:49 -0600

    update banner

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -5,35 +5,27 @@ import { Link } from 'react-scroll';
 const Banner = () => {
     return (
         <BannerLayout>
-            <div className="absolute inset-0 z-20 flex flex-col items-center py-6 justify-center w-full h-full bg-gradient-to-t from-MidNightBlack">
-                <div className="bg-LightGray/10 w-[95%] h-[90%] px-4 py-2 rounded-xl overflow-hidden flex md:block">
-                    <div className="flex items-center md:items-center md:justify-around">
-                        <div className="">
-                            <div className="">
-                                <h1 className="text-3xl sm:text-4xl xl:text-5xl text-Snow font-bold">Hello, Check This Out!</h1>
-                            </div>
-                            <div className="">
-                                <div className=" py-4 font-cascadia-normal text-Snow pb-4 text-xs h-20 lg:h-auto">
-                                    <span>{"<"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>
-                                        {">"} <span className="text-Snow sm:text-xl xl:text-2xl font-bold"> I am a  <span className="inline-block">
-                                            <Typewriter
-                                                options={{
-                                                    strings: [
-                                                        'Developer',
-                                                        'Software Engineer'  // Add any other titles you'd like
-                                                    ],
-                                                    autoStart: true,
-                                                    loop: true,
-                                                }}
-                                            />
-                                        </span>
-                                        </span> {"</"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>{">"} </span>
-                                </div>
-                            </div>
-                            <Link to='intro' spy={true} smooth={true} duration={500} offset={-50} className="button">Explore</Link>
+            <div className="absolute inset-0 z-20 flex flex-col justify-center w-full h-full bg-gradient-to-t from-MidNightBlack">
+                <div className="bg-LightGray/10 w-[95%] h-[90%] px-8 py-2 mx-auto rounded-xl overflow-hidden flex items-center">
+                    <div className="flex flex-col">
+                        <div>
+                            <h1 className="text-3xl sm:text-4xl xl:text-5xl text-Snow font-bold">Hello, Check This Out!</h1>
                         </div>
-                        <div className="w-48 h-52 relative hidden md:block">
-                            <img className='absolute top-8 w-full h-full' src="images/emoji.png" alt="emoji" />
+                        <div className="py-4 font-cascadia-normal text-Snow pb-4 text-xs h-20 lg:h-auto">
+                            <span>{"<"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>
+                                {">"} <span className="text-Snow sm:text-xl xl:text-2xl font-bold"> I am a <span className="inline-block">
+                                    <Typewriter
+                                        options={{
+                                            strings: [
+                                                'Developer',
+                                                'Software Engineer'  // Add any other titles you'd like
+                                            ],
+                                            autoStart: true,
+                                            loop: true,
+                                        }}
+                                    />
+                                </span>
+                                </span> {"</"}<span className="text-Green sm:text-base xl:text-lg font-bold">div</span>{">"} </span>
                         </div>
                     </div>
                 </div>
